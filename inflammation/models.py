654be9aba3286c1f4810aa44d9d9e20e3b25c1b6
elsakente <elsakente@gmail.com>
2022-02-16 15:01:50 +0000

    Add exception raise to patient_normalise function and test models

--- inflammation/models.py
+++ inflammation/models.py
@@ -40,8 +40,8 @@ def patient_normalise(data):
 
     Negative values are rounded to 0.
     """
-#    if np.any(data < 0):
-#        raise ValueError('Inflammation values should not be negative')
+    if np.any(data < 0):
+        raise ValueError('Inflammation values should not be negative')
 
     max_data = np.nanmax(data, axis=1)
     with np.errstate(invalid='ignore', divide='ignore'):
